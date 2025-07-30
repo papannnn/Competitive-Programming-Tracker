28941d0891e35bf67ed482af06037e40aa16ba29
papannn <taufan.maaruf@gmail.com>
2025-07-30 22:14:30 +0700

    codeforces (800) - Cards for Friends

---
+++ codeforces/Problemset/800/160-A. Cards for Friends.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+// O(n * (log w + log h))
+int main () {
+    int n;
+    cin >> n;
+    while (n--) {
+        int w, h, n;
+        cin >> w >> h >> n;
+        int res = 1;
+        while (w % 2 == 0 && w != 0) {
+            w /= 2;
+            res *= 2;
+        }
+
+        while (h % 2 == 0 && h != 0) {
+            h /= 2;
+            res *= 2;
+        }
+
+        cout << (res >= n ? "YES" : "NO") << endl;
+    }
+}
