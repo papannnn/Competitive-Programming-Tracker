0685323f40f701712b39372f7e4420669dcace91
papannn <taufan.maaruf@gmail.com>
2025-07-30 21:55:19 +0700

    codeforces (800) - Clock Conversion

---
+++ codeforces/Problemset/800/159-C. Clock Conversion.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int main () {
+    int n;
+    cin >> n;
+    while (n--) {
+        int hour, min;
+        scanf("%d:%d", &hour, &min);
+        string time;
+        if (hour == 0) {
+            hour = 12;
+            time = "AM";
+        } else if (hour == 12) {
+            time = "PM";
+        } else if (hour > 12) {
+            hour -= 12;
+            time = "PM";
+        } else {
+            time = "AM";
+        }
+
+        if (hour < 10) {
+            cout << "0";
+        }
+        cout << hour << ":";
+
+        if (min < 10) {
+            cout << "0";
+        }
+
+        cout << min << " " << time << endl;
+    }
+}
