eed29235963e7023523818a7ba9abada9ff09bae
papannn <taufan.maaruf@gmail.com>
2025-08-05 21:58:13 +0700

    codeforces (800) - Ichihime and Triangle

---
+++ codeforces/Problemset/800/182-A. Ichihime and Triangle.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+// https://codeforces.com/problemset/problem/1337/A
+// O(t)
+int main () {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long a, b, c, d;
+        cin >> a >> b >> c >> d;
+        cout << b << " " << c << " " << c << endl;        
+    }
+}
