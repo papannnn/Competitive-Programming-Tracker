7bbcab920edcbee9547eeccd98e3210d654d5478
papannn <taufan.maaruf@gmail.com>
2025-09-02 11:13:53 +0700

    codeforces (800) - New World, New Me, New Array

---
+++ codeforces/Problemset/800/222-A. New World, New Me, New Array.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+// https://codeforces.com/problemset/problem/2072/A
+// O(n)
+int main () {
+    int t;
+    cin >> t;
+    while (t--) {
+        double n, k, p;
+        cin >> n >> k >> p;
+        k = abs(k);
+        
+        if (k > n * p) {
+            cout << -1 << endl;
+            continue;
+        }
+
+        int res = ceil(k / p);
+        cout << res << endl;
+    }
+}
