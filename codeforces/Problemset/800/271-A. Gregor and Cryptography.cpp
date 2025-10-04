3ba726bcc8a36d31ea49804d1cb062652d3cd8d5
papannn <taufan.maaruf@gmail.com>
2025-10-04 11:34:46 +0700

    codeforces (800) - Gregor and Cryptography

---
+++ codeforces/Problemset/800/271-A. Gregor and Cryptography.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+// https://codeforces.com/problemset/problem/1549/A
+// O(t)
+int main () {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long n;
+        cin >> n;
+        cout << 2 << " " << n - 1 << endl;
+    }
+}
