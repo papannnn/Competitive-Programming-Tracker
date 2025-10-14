c4e7f3248eb3e15475ced62f86a2bd005030b830
papannn <taufan.maaruf@gmail.com>
2025-10-14 18:45:32 +0700

    codeforces (800) - Strong Password

--- codeforces/Problemset/800/305-A. Strong Password.cpp
+++ codeforces/Problemset/800/305-A. Strong Password.cpp
@@ -2,14 +2,29 @@
 
 using namespace std;
 // https://codeforces.com/problemset/problem/1997/A
-// O(t)
+// O(t * s)
 int main () {
     int t;
     cin >> t;
     while (t--) {
         string s;
         cin >> s;
-        char res = s[0] == 'a' ? 'b' : 'a';
-        cout << s << res << endl;
+        
+        bool found = false;
+        for (int i = 0 ; i < s.length() - 1; i++) {
+            cout << s[i];
+            char otherC = s[i] == 'a' ? 'b' : 'a';
+            if (!found && s[i] == s[i + 1]) {
+                cout << otherC;
+                found = true;
+            }
+        }
+        cout << s[s.length() - 1];
+
+        if (!found) {
+            char otherC = s[s.length() - 1] == 'a' ? 'b' : 'a';
+            cout << otherC;
+        }
+        cout << endl;
     }
 }
