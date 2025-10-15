7850abb8d04a8983a140d56eedebd570d2aac72d
papannn <taufan.maaruf@gmail.com>
2025-10-15 22:56:47 +0700

    codeforces (800) - Squares and Cubes

---
+++ codeforces/Problemset/800/312-B. Squares and Cubes.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+// https://codeforces.com/problemset/problem/1619/B
+// O(t)
+int main () {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        unordered_set<int> num;
+        long long val = 1;
+        while (val * val <= n) {
+            num.insert(val * val);
+            if (val * val * val <= n) {
+                num.insert(val * val * val);
+            }
+            val++;
+        }
+        cout << num.size() << endl;
+    }
+}
