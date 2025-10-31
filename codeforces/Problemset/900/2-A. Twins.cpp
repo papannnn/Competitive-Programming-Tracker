632e5dffe485974ab6664993075d45235e0a65f5
papannn <taufan.maaruf@gmail.com>
2025-10-31 20:50:07 +0700

    codeforces (900) - Twins

---
+++ codeforces/Problemset/900/2-A. Twins.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+// https://codeforces.com/problemset/problem/160/A
+// O(t * n log n)
+
+void merge(vector<int> &arr, int l, int mid, int r) {
+    int ptr1 = l;
+    int ptr2 = mid + 1;
+
+    vector<int> temp;
+    while (ptr1 <= mid && ptr2 <= r) {
+        if (arr[ptr1] > arr[ptr2]) {
+            temp.push_back(arr[ptr1++]);
+        } else {
+            temp.push_back(arr[ptr2++]);
+        }
+    }
+
+    while (ptr1 <= mid) {
+        temp.push_back(arr[ptr1++]);
+    }
+
+    while (ptr2 <= r) {
+        temp.push_back(arr[ptr2++]);
+    }
+
+    for (int i = 0 ; i < temp.size(); i++) {
+        arr[l + i] = temp[i];
+    }
+}
+
+void mergeSort(vector<int> &arr, int l, int r) {
+    if (l < r) {
+        int mid = (l + r) / 2;
+        mergeSort(arr, l, mid);
+        mergeSort(arr, mid + 1, r);
+        merge(arr, l, mid, r);
+    }
+}
+
+int main () {
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    int tot = 0;
+    for (int &num : arr) {
+        cin >> num;
+        tot += num;
+    }
+    mergeSort(arr, 0, arr.size() - 1);
+    int curr = 0;
+    int idx = 0;
+    while (curr <= (tot / 2)) {
+        curr += arr[idx++];
+    }
+    cout << idx << endl;
+}
