8e2c45bfccc053b3e3da8ed8bd57d52c4b119d79
papannn <taufan.maaruf@gmail.com>
2025-10-31 22:46:48 +0700

    fix

--- codeforces/Problemset/900/1-A. Football.cpp
+++ codeforces/Problemset/900/1-A. Football.cpp
@@ -7,17 +7,17 @@ int main () {
     string s;
     cin >> s;
 
-    int streak = 0;
-    int maxStreak = 0;
+    int streak = 1;
+    int maxStreak = 1;
     char curr = s[0];
     for (int i = 1 ; i < s.length(); i++) {
         if (s[i] == curr) {
             streak++;
-            maxStreak = max(maxStreak, streak);
         } else {
             streak = 1;
             curr = s[i];
         }
+        maxStreak = max(maxStreak, streak);
     }
 
     cout << (maxStreak >= 7 ? "YES" : "NO") << endl;
