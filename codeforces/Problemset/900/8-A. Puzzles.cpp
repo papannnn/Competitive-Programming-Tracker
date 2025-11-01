c3227c0e219442622c564b321d21da36ffe3a84d
papannn <taufan.maaruf@gmail.com>
2025-11-01 13:31:14 +0700

    codeforces (900) - Puzzles

---
+++ codeforces/Problemset/900/8-A. Puzzles.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+// https://codeforces.com/problemset/problem/337/A
+// O(n log n + m)
+void merge(vector<int> &arr, int l, int mid, int r) {
+    int ptr1 = l;
+    int ptr2 = mid + 1;
+
+    vector<int> temp;
+    while (ptr1 <= mid && ptr2 <= r) {
+        if (arr[ptr1] < arr[ptr2]) {
+            temp.push_back(arr[ptr1++]);
+        } else {
+            temp.push_back(arr[ptr2++]);
+        }
+    }
+
+    while (ptr1 <= mid) {
+        temp.push_back(arr[ptr1++]);
+    }
+
+    while (ptr2 <= r) {
+        temp.push_back(arr[ptr2++]);
+    }
+
+    for (int i = 0 ; i < temp.size(); i++) {
+        arr[l + i] = temp[i];
+    }
+}
+
+void mergeSort(vector<int> &arr, int l, int r) {
+    if (l < r) {
+        int mid = (l + r) / 2;
+        mergeSort(arr, l, mid);
+        mergeSort(arr, mid + 1, r);
+        merge(arr, l, mid, r);
+    }
+}
+
+int main () {
+    int n, m;
+    cin >> n >> m;
+    vector<int> arr(m);
+    for (int &num : arr) {
+        cin >> num;
+    }
+
+    mergeSort(arr, 0, arr.size() - 1);
+    int res = 1e9;
+    for (int i = 0 ; i < arr.size() - (n - 1); i++) {
+        res = min(res, abs(arr[i] - arr[i + n - 1]));
+    }
+    cout << res << endl;
+}
