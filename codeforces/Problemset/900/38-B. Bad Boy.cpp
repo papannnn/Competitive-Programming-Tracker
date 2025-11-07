4302705511f1426169b035032890d8cb294880f7
papannn <taufan.maaruf@gmail.com>
2025-11-07 23:34:02 +0700

    codeforces (900) - Bad Boy

---
+++ codeforces/Problemset/900/38-B. Bad Boy.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+// https://codeforces.com/problemset/problem/1537/B
+// O(t)
+int main () {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m, y, x;
+        cin >> n >> m >> y >> x;
+        cout << 1 << " " << 1 << " " << n << " " << m << endl;
+    }
+}
