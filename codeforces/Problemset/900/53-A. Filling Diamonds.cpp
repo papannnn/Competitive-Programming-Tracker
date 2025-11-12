86298cb31888e625fecba5fc500754e4b7993b5d
papannn <taufan.maaruf@gmail.com>
2025-11-12 16:26:08 +0700

    codeforces (900) - Filling Diamonds

---
+++ codeforces/Problemset/900/53-A. Filling Diamonds.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+// https://codeforces.com/problemset/problem/1339/A
+// O(t)
+int main () {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long n;
+        cin >> n;
+        cout << n << endl;
+    }
+}
