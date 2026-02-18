17791c0ea840667d3d858f18e81ff2cc301ce97d
papannn <taufan.maaruf@gmail.com>
2026-02-18 15:41:30 +0700

    refactor

--- ThemeCP/91-Level 11 (Attempt 5)/3-B. Outstanding Impressionist.cpp
+++ ThemeCP/91-Level 11 (Attempt 5)/3-B. Outstanding Impressionist.cpp
@@ -9,45 +9,35 @@ int main () {
     while (t--) {
         int n;
         cin >> n;
-        map<pair<int, int>, int> mapping;
-        vector<pair<int, int>> arr;
-        set<int> single;
+        vector<int> lArr(n);
+        vector<int> rArr(n);
+        vector<int> flag(2 * n + 1);
+        vector<int> single;
         for (int i = 0 ; i < n; i++) {
             int l, r;
             cin >> l >> r;
             int size = r - l + 1;
-            if (size == 1) {
-                single.insert(l);
+            if (size == 1 && flag[r] == 0) {
+                single.push_back(r);
             }
-            auto p = make_pair(l, r);
-            arr.push_back(p);
-            mapping[p]++;
-        }
-
-        vector<int> singleArr;
-        for (auto &n : single) {
-            singleArr.push_back(n);
-        }
-        sort(singleArr.begin(), singleArr.end());
-        for (int i = 0 ; i < arr.size(); i++) {
-            int size = arr[i].second - arr[i].first + 1;
             if (size == 1) {
-                if (mapping[arr[i]] == 1) {
-                    cout << 1;
-                } else {
-                    cout << 0;
-                }
+                flag[r]++;
+            }
+            lArr[i] = l;
+            rArr[i] = r;
+        }
+        sort(single.begin(), single.end());
+        for (int i = 0 ; i < n; i++) {
+            int size = rArr[i] - lArr[i] + 1;
+            if (size == 1) {
+                cout << (flag[rArr[i]] == 1);
                 continue;
             }
 
-            auto it1 = lower_bound(singleArr.begin(), singleArr.end(), arr[i].first);
-            auto it2 = upper_bound(singleArr.begin(), singleArr.end(), arr[i].second);
-            int count = it2 - it1;
-            if (count == size) {
-                cout << 0;
-            } else {
-                cout << 1;
-            }
+            auto it1 = lower_bound(single.begin(), single.end(), lArr[i]);
+            auto it2 = upper_bound(single.begin(), single.end(), rArr[i]);
+            int cnt = it2 - it1;
+            cout << (cnt == size ? 0 : 1);
         }
         cout << endl;
     }
