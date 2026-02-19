ea83d7f8624391482bdc7f4c1bee2b47ff1b429f
papannn <taufan.maaruf@gmail.com>
2026-02-20 01:25:13 +0700

    upsolve

--- ThemeCP/92-Level 12 (Attempt 3)/1-A. Binary Matrix.cpp
+++ ThemeCP/92-Level 12 (Attempt 3)/1-A. Binary Matrix.cpp
@@ -16,5 +16,23 @@ int main () {
             }
         }
 
+        int r = 0;
+        for (int i = 0 ; i < n; i++) {
+            int tot = 0;
+            for (int j = 0; j < m; j++) {
+                tot += arr[i][j] == '1';
+            }
+            r += tot % 2;
+        }
+
+        int c = 0;
+        for (int i = 0; i < m; i++) {
+            int tot = 0;
+            for (int j = 0; j < n; j++) {
+                tot += arr[j][i] == '1';
+            }
+            c += tot % 2;
+        }
+        cout << max(r, c) << endl;
     }
 }
