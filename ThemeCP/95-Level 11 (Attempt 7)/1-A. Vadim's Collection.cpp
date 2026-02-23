018cd879ea1f20d4cf78946660bb8e24491089c5
papannn <taufan.maaruf@gmail.com>
2026-02-23 09:40:30 +0700

    upsolve

--- ThemeCP/95-Level 11 (Attempt 7)/1-A. Vadim's Collection.cpp
+++ ThemeCP/95-Level 11 (Attempt 7)/1-A. Vadim's Collection.cpp
@@ -2,49 +2,28 @@
 
 using namespace std;
 // https://codeforces.com/problemset/problem/2098/A
-// O(t * s)
+// O(t)
 int main () {
     int t;
     cin >> t;
-    // char c = '9' - 0;
-    // cout << c << endl;
     while (t--) {
         string s;
         cin >> s;
         unordered_map<char, int> mapping;
-        string res = "xxxxxxxxxx";
         int limit = 9;
         for (int i = 0 ; i < s.length(); i++) {
             mapping[s[i]]++;
         }
 
-        for (auto &m : mapping) {
-            cout << m.first << " " << m.second << endl;
-        }
-
-        for (int i = 0; i <= 9; i++) {
-            char c = '9' - i;
-            if (mapping[c]) {
-                res[i] = '9' - i;
-                mapping['9' - i]--;
-            }
-        }
-
         for (int i = 9; i >= 0; i--) {
-            if (res[i] != 'x') {
-                continue;
-            }
-
-            for (int j = 9; j >= 0; j--) {
-                char c = '9' - j;
-                if (mapping[c]) {
-                    res[i] = '9' - j;
-                    mapping['9' - j]--;
+            for (int j = 0; j <= 9; j++) {
+                if (j >= i && mapping['0' + j]) {
+                    mapping['0' + j]--;
+                    cout << j;
+                    break;
                 }
-                break;
             }
         }
-
-        cout << res << endl;
+        cout << endl;
     }
 }
--- ThemeCP/95-Level 11 (Attempt 7)/2-A. Add and Divide.cpp
+++ ThemeCP/95-Level 11 (Attempt 7)/2-A. Add and Divide.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+// https://codeforces.com/problemset/problem/1485/A
 // O(t * log n)
 int main () {
     int t;
@@ -18,12 +19,20 @@ int main () {
             continue;
         }
 
-        bool one = false;
-        if (b == 1) {
-            one = true;
-            b++;
-        }
+        long long res = 1e9;
+        for (int i = 0; i < 6; i++) {
+            if (b == 1 && i == 0) {
+                continue;
+            }
 
-        long long prev = 
+            long long temp = a;
+            long long cnt = 0;
+            while (temp) {
+                temp /= b + i;
+                cnt++;
+            }
+            res = min(res, cnt + i);
+        }
+        cout << res << endl;
     }
 }
