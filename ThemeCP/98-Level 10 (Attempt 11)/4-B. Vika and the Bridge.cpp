bf0be1299d8c14036551f030b60ef47fe8281a5a
papannn <taufan.maaruf@gmail.com>
2026-02-25 21:59:48 +0700

    upsolve

--- ThemeCP/98-Level 10 (Attempt 11)/4-B. Vika and the Bridge.cpp
+++ ThemeCP/98-Level 10 (Attempt 11)/4-B. Vika and the Bridge.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 // https://codeforces.com/problemset/problem/1848/B
-// O(t * n)
+// O(t * n log n)
 int main () {
     int t;
     cin >> t;
@@ -19,27 +19,36 @@ int main () {
             continue;
         }
 
-        int res = 1e9;
-        for (int i = 1; i <= n; i++) {
-            for (int j = i; j + i <= n; j++) {
-                int x = 0;
-                bool fail = false;
-                for (int k = i; k <= n; k += j) {
-                    if (arr[k] != arr[i]) {
-                        x++;
-                    }
+        unordered_map<int, vector<int>> mapping;
+        for (int i = 1; i <= k; i++) {
+            mapping[i].push_back(0);
+        }
 
-                    if (x == 2) {
-                        fail = true;
-                        break;
-                    }
-                    // cout << arr[k] << " ";
-                }
-                // cout << endl;
-                // cout << j - 1 << endl << endl;
-                if (!fail)
-                res = min(res, j - 1);
+        for (int i = 1; i <= n; i++) {
+            mapping[arr[i]].push_back(i);
+        }
+
+        for (int i = 1; i <= k; i++) {
+            mapping[i].push_back(n + 1);
+        }
+
+        int res = 1e9;
+        for (auto &m : mapping) {
+            priority_queue<int> dist;
+            for (int i = 0; i < m.second.size() - 1; i++) {
+                dist.push(m.second[i + 1] - m.second[i] - 1);
             }
+
+            int maxDist = dist.top();
+            dist.pop();
+            if (maxDist % 2) {
+                dist.push(maxDist / 2);
+                dist.push((maxDist / 2) - 1);
+            } else {
+                dist.push(maxDist / 2);
+                dist.push(maxDist / 2);
+            }
+            res = min(res, dist.top());
         }
         cout << res << endl;
     }
