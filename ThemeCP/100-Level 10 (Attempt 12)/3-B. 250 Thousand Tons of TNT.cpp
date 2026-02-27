2f075f6d767efe9391de0c2dc94d10b481bcd947
papannn <taufan.maaruf@gmail.com>
2026-02-27 23:54:16 +0700

    upsolve

--- ThemeCP/100-Level 10 (Attempt 12)/3-B. 250 Thousand Tons of TNT.cpp
+++ ThemeCP/100-Level 10 (Attempt 12)/3-B. 250 Thousand Tons of TNT.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 // https://codeforces.com/problemset/problem/1899/B
-// O(t * n)
+// O(t * n sqrt(n))
 int main () {
     int t;
     cin >> t;
@@ -10,21 +10,31 @@ int main () {
         int n;
         cin >> n;
 
+        vector<int> arr(n);
+        for (int &num : arr) {
+            cin >> num;
+        }
 
+        long long res = 0;
         for (int i = 1 ; i <= n; i++) {
             if (n % i) {
                 continue;
             }
 
-            // for (int j = 0; j < n; j += i) {
-            //     for (int k = )
-            // }
-        }
+            long long maxVal = -1e18;
+            long long minVal = 1e18;
+            
+            for (int j = 0; j < n; j += i) {
+                long long sum = 0;
+                for (int k = j; k < j + i; k++) {
+                    sum += arr[k];
+                }
+                maxVal = max(maxVal, sum);
+                minVal = min(minVal, sum);
+            }
 
-        vector<int> arr(n);
-        for (int &num : arr) {
-            cin >> num;
+            res = max(res, maxVal - minVal);
         }
-        cout << endl;
+        cout << res << endl;
     }
 }
