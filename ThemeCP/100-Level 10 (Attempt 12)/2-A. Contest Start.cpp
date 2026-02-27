52b6e50e04637f47e0b06bd177a25bc619a781a9
papannn <taufan.maaruf@gmail.com>
2026-02-28 00:58:49 +0700

    upsolve

--- ThemeCP/100-Level 10 (Attempt 12)/2-A. Contest Start.cpp
+++ ThemeCP/100-Level 10 (Attempt 12)/2-A. Contest Start.cpp
@@ -9,6 +9,7 @@ int main () {
     while (t--) {
         long long n, x, t;
         cin >> n >> x >> t;
-        
+        long long m = min(n, t / x);
+        cout << (m * (2 * n - 1 - m) / 2) << endl;   
     }
 }
