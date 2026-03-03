63d312ab0216ffe586a831c1f3625efff3c03798
papannn <taufan.maaruf@gmail.com>
2026-03-03 18:31:19 +0700

    TLE Eliminator - Level 1 - Day 1 - Welcome for you with Conditions

---
+++ TLE Eliminator/Level 1/1-C++ Basics/Day 1/1-I. Welcome for you with Conditions.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+// https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/I
+// O(1)
+int main () {
+    int a, b;
+    cin >> a >> b;
+    cout << (a >= b ? "Yes" : "No") << endl;
+}
