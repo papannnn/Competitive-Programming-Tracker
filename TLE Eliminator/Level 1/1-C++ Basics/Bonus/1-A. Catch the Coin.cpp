74937bf367726fec4070178f46e470cc6a09e739
papannn <taufan.maaruf@gmail.com>
2026-03-06 18:15:34 +0700

    ThemeCP - Level 1 - C++ Basics - Bonus 1

---
+++ TLE Eliminator/Level 1/1-C++ Basics/Bonus/1-A. Catch the Coin.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+// https://codeforces.com/problemset/problem/1989/A
+// O(1)
+int main () {
+    int t;
+    cin >> t;
+    while (t--) {
+        int posX, posY;
+        cin >> posX >> posY;
+        int x = 0;
+        int y = 0;
+        
+        if (posY < y - 1) {
+            cout << "NO" << endl;
+            continue;
+        }
+
+        cout << "YES" << endl;
+    }
+}
