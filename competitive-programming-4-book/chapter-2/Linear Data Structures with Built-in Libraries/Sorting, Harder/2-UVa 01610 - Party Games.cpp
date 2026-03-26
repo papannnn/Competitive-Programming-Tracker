9f8067642fa513eb33dff5121ff150adbb046371
papannn <taufan.maaruf@gmail.com>
2026-03-27 00:17:38 +0700

    UVa 01610 - Party Games

--- competitive-programming-4-book/chapter-2/Linear Data Structures with Built-in Libraries/Sorting, Harder/2-UVa 01610 - Party Games.cpp
+++ competitive-programming-4-book/chapter-2/Linear Data Structures with Built-in Libraries/Sorting, Harder/2-UVa 01610 - Party Games.cpp
@@ -1,9 +1,9 @@
 #include <bits/stdc++.h>
-
 using namespace std;
+
 // O(t * s log s)
 // https://vjudge.net/problem/UVA-1610
-int main () {
+int main() {
     int n;
     while (true) {
         cin >> n;
@@ -15,27 +15,56 @@ int main () {
         for (string &s : arr) {
             cin >> s;
         }
+
         sort(arr.begin(), arr.end());
+
         int idx = arr.size() / 2 - 1;
         string res = "";
         int lastIdx = 0;
-        // bool 
-        for (int i = 0 ; i < min(arr[idx].length(), arr[idx + 1].length()); i++) {
+
+        for (int i = 0; i < min(arr[idx].length(), arr[idx + 1].length()); i++) {
             if (arr[idx][i] == arr[idx + 1][i]) {
                 res += arr[idx][i];
                 lastIdx++;
                 continue;
             }
 
-            if (arr[idx][i] + 1 == arr[idx + 1][i] && (i == arr[idx + 1].size() - 1 || i != arr[idx].size() - 1)) {
+            if (arr[idx][i] + 1 != arr[idx + 1][i] && i == arr[idx].size() - 1) {
                 res += arr[idx][i];
                 lastIdx++;
                 break;
             }
 
-            res += arr[idx][i] + 1;
+            if (arr[idx][i] + 1 != arr[idx + 1][i]) {
+                res += arr[idx][i] + 1;
+                lastIdx++;
+                break;
+            }
+
+            if (arr[idx][i] + 1 == arr[idx + 1][i] && i == arr[idx].size() - 1) {
+                res += arr[idx][i];
+                lastIdx++;
+                break;
+            }
+
+            if (arr[idx][i] + 1 == arr[idx + 1][i] &&
+                i != arr[idx + 1].size() - 1) {
+                res += arr[idx][i] + 1;
+                lastIdx++;
+                break;
+            }
+
+            res += arr[idx][i];
             lastIdx++;
-            break;
+        }
+
+        while (lastIdx < arr[idx].size() - 1 && res < arr[idx]) {
+            if (arr[idx][lastIdx] == 'Z') {
+                res += arr[idx][lastIdx];
+            } else {
+                res += arr[idx][lastIdx] + 1;
+            }
+            lastIdx++;
         }
 
         while (lastIdx < arr[idx].size() && res < arr[idx]) {
