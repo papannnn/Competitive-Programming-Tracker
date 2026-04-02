bbe430366e7acd95eb53e4978d058c64964ddb66
papannn <taufan.maaruf@gmail.com>
2026-04-02 20:43:58 +0700

    Kokocoder roadmap - Takahashi

---
+++ kokocoder/Roadmap/Green-1-1 🟢 Input and Output/2-Anyway Takahashi.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+// O(1)
+int main () {
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    cout << (a + b) * (c - d) << endl;
+    cout << "Takahashi" << endl;
+}
