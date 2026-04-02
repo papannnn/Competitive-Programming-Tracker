33c28c7fc3215ba88b5b5a88695a840e16622505
papannn <taufan.maaruf@gmail.com>
2026-04-02 22:12:31 +0700

    Kokocoder roadmap - Calc

---
+++ kokocoder/Roadmap/Green-1-2 🟢 Arithmetic Operator/5-Calc.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+// O(1)
+int main () {
+    int a;
+    cin >> a;
+    int res = 0;
+    res += a;
+    res += a * a;
+    res += a * a * a;
+    cout << res << endl;
+}
