baf05016500bea3576267fcb5243299a541dfa55
papannn <taufan.maaruf@gmail.com>
2026-04-02 20:39:47 +0700

    Kokocoder roadmap - Batting Average

---
+++ kokocoder/Roadmap/Green-1-0 🟢 Data Types I/2-Batting Average.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+// O(1)
+int main () {
+    double a, b;
+    cin >> a >> b;
+    double res = b / a;
+    cout << fixed << setprecision(3) << res << endl;
+}
