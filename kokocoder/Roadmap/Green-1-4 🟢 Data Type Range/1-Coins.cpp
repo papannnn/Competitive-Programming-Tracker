a3d6aca9f8b3889b6d292fbc8ed97cd9ba6f20eb
papannn <taufan.maaruf@gmail.com>
2026-04-03 18:38:50 +0700

    Kokocoder roadmap - Coins

---
+++ kokocoder/Roadmap/Green-1-4 🟢 Data Type Range/1-Coins.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+// O(t)
+int main () {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long n, k;
+        cin >> n >> k;
+
+        if (n % 2 == 0) {
+            cout << "YES" << endl;
+        } else {
+            if (k % 2 == 0) {
+                cout << "NO" << endl;
+            } else {
+                cout << "YES" << endl;
+            }
+        }
+    }
+}
