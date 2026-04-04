fa3b8502076d39abecf87553ef71805a5ab9ac6c
papannn <taufan.maaruf@gmail.com>
2026-04-04 12:21:52 +0700

    Kokocoder roadmap - Print 341

---
+++ kokocoder/Roadmap/Green-2/Green-2-2 🟢 Looping II - For/8-Print 341.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+// O(n)
+int main () {
+    int n;
+    cin >> n;
+    cout << 1;
+    for (int i = 0; i < n; i++) {
+        cout << "01";
+    }
+    cout << endl;
+}
