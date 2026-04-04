6cb73a5a32b9f370e5a23f9bf903aa7bfcd8d1ce
papannn <taufan.maaruf@gmail.com>
2026-04-04 11:03:30 +0700

    Kokocoder roadmap - Very Very Primitive Game

---
+++ kokocoder/Roadmap/Green-2/Green-2-1 🟢 Looping I - While/7-Very Very Primitive Game.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+// O(1)
+int main () {
+    int a, b, c;
+    cin >> a >> b >> c;
+    if (c == 0) {
+        if (a <= b) {
+            cout << "Aoki" << endl;
+        } else {
+            cout << "Takahashi" << endl;
+        }
+    } else {
+        if (a >= b) {
+            cout << "Takahashi" << endl;
+        } else {
+            cout << "Aoki" << endl;
+        }
+    }
+}
