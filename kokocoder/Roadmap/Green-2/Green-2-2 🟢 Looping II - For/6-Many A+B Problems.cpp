24acc9caea522f5efd4876f6790dbeaa3c56be63
papannn <taufan.maaruf@gmail.com>
2026-04-04 12:06:38 +0700

    Kokocoder roadmap - Many A+B Problems

---
+++ kokocoder/Roadmap/Green-2/Green-2-2 🟢 Looping II - For/6-Many A+B Problems.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+// O(n)
+int main () {
+    int n;
+    cin >> n;
+    while (n--) {
+        long long a, b;
+        cin >> a >> b;
+        cout << a + b << endl;
+    }
+}
