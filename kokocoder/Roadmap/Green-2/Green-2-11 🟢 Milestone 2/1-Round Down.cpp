d0b9084ff6c3ae7f0301e4ad33c9ffb665325322
papannn <taufan.maaruf@gmail.com>
2026-04-06 13:21:32 +0700

    Kokocoder roadmap - Round Down

---
+++ kokocoder/Roadmap/Green-2/Green-2-11 🟢 Milestone 2/1-Round Down.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+// O(s)
+int main () {
+    string s;
+    cin >> s;
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == '.') {
+            break;
+        }
+        cout << s[i];
+    }
+}
