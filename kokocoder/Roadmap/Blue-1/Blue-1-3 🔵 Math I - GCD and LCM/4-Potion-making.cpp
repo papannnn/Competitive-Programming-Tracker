3397628a593037907c358e0440b84c324d7377b3
papannn <taufan.maaruf@gmail.com>
2026-04-28 21:27:12 +0700

    Kokocoder roadmap - Potion-making

---
+++ kokocoder/Roadmap/Blue-1/Blue-1-3 🔵 Math I - GCD and LCM/4-Potion-making.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int gcd(int a, int b) {
+    if (b == 0) {
+        return a;
+    }
+    return gcd(b, a % b);
+}
+
+int main () {
+    int t;
+    cin >> t;
+    while (t--) {
+        int num;
+        cin >> num;
+        int lcm = 100 * num / gcd(100, num);
+        cout << lcm / num << endl;
+    }
+}
