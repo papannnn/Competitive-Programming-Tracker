366fb8bd1a2edccaeffc4bcb40ed1c797a99159a
papannn <taufan.maaruf@gmail.com>
2026-05-03 00:44:48 +0700

    tlx Jawbreaker II: Cari Terbesar

---
+++ tlx/Pemrograman Kompetitif Dasar/04. Brute Force/E. Jawbreaker II: Cari Terbesar.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int pop(vector<vector<int>> &arr, int b, int k, int target, vector<vector<bool>> &visited) {
+    if (b == -1 || k == -1 || b == arr.size() || k == arr[0].size()) {
+        return 0;
+    }
+
+    if (arr[b][k] != target) {
+        return 0;
+    }
+
+    if (visited[b][k]) {
+        return 0;
+    }
+
+    visited[b][k] = true;
+    return 1 + pop(arr, b + 1, k, target, visited)
+     + pop(arr, b - 1, k, target, visited) 
+     + pop(arr, b, k + 1, target, visited) 
+     + pop(arr, b, k - 1, target, visited);
+}
+
+int main () {
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> arr(n, vector<int>(m));
+    for (vector<int> &a : arr) {
+        for (int &num : a) {
+            cin >> num;
+        }
+    }
+
+    int res = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            vector<vector<bool>> visited(n, vector<bool>(m));
+            int val = pop(arr, i, j, arr[i][j], visited);
+            res = max(res, val);
+        }
+    }
+    
+    cout << (res * (res - 1)) << endl;
+}
