1df173404ec6e5a81ba70f6cf5638e877391d539
papannn <taufan.maaruf@gmail.com>
2026-05-04 02:12:26 +0700

    partially done Jawbreaker IV

---
+++ tlx/Pemrograman Kompetitif Dasar/04. Brute Force/G. Jawbreaker IV: Cari Terbesar 2 Langkah.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int pop(vector<vector<char>> &arr, int b, int k, char target, vector<vector<bool>> &visited) {
+    if (b == -1 || k == -1 || b == arr.size() || k == arr[0].size()) {
+        return 0;
+    }
+
+    if (arr[b][k] != target) {
+        return 0;
+    }
+
+    if (visited[b][k]) {
+        return 0;
+    }
+
+    visited[b][k] = true;
+    return 1 + pop(arr, b + 1, k, target, visited)
+     + pop(arr, b - 1, k, target, visited) 
+     + pop(arr, b, k + 1, target, visited) 
+     + pop(arr, b, k - 1, target, visited);
+}
+
+int main () {
+    int n, m;
+    cin >> n >> m;
+    vector<vector<char>> arr(n, vector<char>(m));
+    for (vector<char> &a : arr) {
+        for (char &num : a) {
+            cin >> num;
+        }
+    }
+
+    long long res = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            vector<vector<bool>> visited(n, vector<bool>(m));
+            vector<vector<char>> temp(arr);
+            long long val = pop(temp, i, j, temp[i][j], visited);
+
+            for (int k = 0 ; k < temp.size(); k++) {
+                for (int l = 0; l < temp[k].size(); l++) {
+                    if (visited[k][l]) {
+                        temp[k][l] = '.';
+                    }
+                }
+            }
+
+            for (int k = 0; k < m; k++) {
+                for (int l = 0; l < n - 1; l++) {
+                    for (int z = 0; z < n - l - 1; z++) {
+                        if (temp[z + 1][k] == '.') {
+                            swap(temp[z][k], temp[z + 1][k]);
+                        }
+                    }
+                }
+            }
+
+            for (int k = 0; k < n; k++) {
+                for (int l = 0; l < m; l++) {
+                    if (temp[k][l] == '.') {
+                        continue;
+                    }
+                    vector<vector<bool>> visited(n, vector<bool>(m));
+                    long long val1 = pop(temp, k, l, temp[k][l], visited);
+                    res = max(res, (val1 * (val1 - 1)) + (val * (val - 1)));
+                }
+            }
+        }
+    }
+    cout << res << endl;
+}
