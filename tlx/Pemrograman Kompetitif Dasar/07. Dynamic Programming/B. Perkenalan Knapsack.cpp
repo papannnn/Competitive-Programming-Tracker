2b4dfb0f66aa20c44df35666f22e6b97c1bc2a68
papannn <taufan.maaruf@gmail.com>
2026-05-20 20:05:18 +0700

    bottom up knapsack

--- tlx/Pemrograman Kompetitif Dasar/07. Dynamic Programming/B. Perkenalan Knapsack.cpp
+++ tlx/Pemrograman Kompetitif Dasar/07. Dynamic Programming/B. Perkenalan Knapsack.cpp
@@ -2,67 +2,33 @@
 
 using namespace std;
 
-struct Batu {
-    long long berat;
-    long long harga;
+struct Item {
+    long long weight;
+    long long value;
 };
-
-pair<long long,long long> solve(long long idx, vector<Batu> &arr, long long n, vector<vector<pair<long long,long long>>> &memo) {
-    if (idx == (long long)arr.size()) return {0, 0};
-    if (memo[n][idx].first != -1) return memo[n][idx];
-
-    pair<long long,long long> skip = solve(idx + 1, arr, n, memo);
-
-    pair<long long,long long> take = {-1, 0};
-    if (arr[idx].berat <= n) {
-        auto [v, w] = solve(idx + 1, arr, n - arr[idx].berat, memo);
-        take = {v + arr[idx].harga, w + arr[idx].berat};
-    }
-
-    pair<long long,long long> best;
-    if (take.first > skip.first)
-        best = take;
-    else if (take.first == skip.first && take.second <= skip.second)
-        best = take;
-    else
-        best = skip;
-
-    return memo[n][idx] = best;
-}
 
 int main () {
     long long n, k;
-    cin >> n >> k;
-    vector<Batu> arr(k);
-    for (Batu &b : arr) {
-        cin >> b.berat >> b.harga;
+    cin >> k >> n;
+    vector<Item> arr(n);
+    for (Item &i : arr) {
+        cin >> i.weight >> i.value;
     }
 
-    vector<vector<pair<long long, long long>>> memo(n + 1, vector<pair<long long, long long>>(k + 1, {-1, -1}));
-    pair<long long, long long> state = {0, 0};
-    solve(0, arr, n, memo);
-    // cout << res.first << " " << res.second << endl;
+    vector<vector<long long>> memo(n + 1, vector<long long>(k + 1, 0));
+    for (int i = 1 ; i <= n; i++) {
+        for (int j = 1 ; j <= k; j++) {
+            long long pick = 0;
 
-    vector<long long> res;
-    for (long long i = 0; i < (long long)arr.size(); i++) {
-        if (n >= arr[i].berat) {
-            auto [tv, tw] = solve(i + 1, arr, n - arr[i].berat, memo);
-            tv += arr[i].harga;
-            tw += arr[i].berat;
-            auto [sv, sw] = solve(i + 1, arr, n, memo);
-
-            bool prefer_take = false;
-            if (tv > sv) prefer_take = true;
-            else if (tv == sv && tw <= sw) prefer_take = true;
-
-            if (prefer_take) {
-                n -= arr[i].berat;
-                res.push_back(i + 1);
+            if (arr[i - 1].weight <= j) {
+                pick = arr[i - 1].value
+                    + memo[i - 1][j - arr[i - 1].weight];
             }
+
+            long long notPick = memo[i - 1][j];
+
+            memo[i][j] = max(pick, notPick);
         }
     }
-
-    for (long long &num : res) {
-        cout << num << endl;
-    }
+    cout << memo[n][k] << endl;
 }
