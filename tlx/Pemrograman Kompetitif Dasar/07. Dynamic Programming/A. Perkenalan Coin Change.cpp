a8b654f00448c70307909c9d2da9c8ea9d196a1e
papannn <taufan.maaruf@gmail.com>
2026-05-20 18:51:14 +0700

    bottom up coin change

--- tlx/Pemrograman Kompetitif Dasar/07. Dynamic Programming/A. Perkenalan Coin Change.cpp
+++ tlx/Pemrograman Kompetitif Dasar/07. Dynamic Programming/A. Perkenalan Coin Change.cpp
@@ -1,45 +1,30 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
-long long solve(int idx, vector<int> &koin, int k, vector<int> &memo) {
-    if (k == 0) {
-        return 0;
-    }
-
-    if (idx == koin.size()) {
-        return INT_MAX;
-    }
-
-    if (memo[k] != -1) {
-        return memo[k];
-    }
-
-    long long res = INT_MAX;
-    long long take = INT_MAX;
-    if (koin[idx] <= k) {
-        take = solve(idx, koin, k - koin[idx], memo) + 1;
-    }
-    memo[k] = min(take, min(res, solve(idx + 1, koin, k, memo)));;
-    return memo[k];
-}
-
 int main () {
     int n;
     cin >> n;
-    vector<int> koin(n);
-    
-    for (int &num : koin) {
+    vector<int> coins(n);
+    for (int &num : coins) {
         cin >> num;
     }
 
     int k;
     cin >> k;
-    vector<int> memo(k + 1, -1);
-    long long res = solve(0, koin, k, memo);
-    if (res == INT_MAX) {
+    vector<int> memo(k + 1, 0);
+
+    for (int i = 1; i <= k ; i++) {
+        int best = 1e9;
+        for (int j = 0; j < n; j++) {
+            if (coins[j] <= i) {
+                best = min(best, memo[i - coins[j]] + 1);
+            }
+        }
+        memo[i] = best;
+    }
+    if (memo[k] == 1e9) {
         cout << -1 << endl;
     } else {
-        cout << solve(0, koin, k, memo) << endl;
+        cout << memo[k] << endl;
     }
 }
