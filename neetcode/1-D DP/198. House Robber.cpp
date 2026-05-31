771dee7cce0922080b8a8748c89cc62e1ba3749a
papannn <taufan.maaruf@gmail.com>
2026-05-31 12:07:21 +0700

    House Robber

---
+++ neetcode/1-D DP/198. House Robber.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if (nums.size() == 1) {
+            return nums[0];
+        }
+
+        if (nums.size() == 2) {
+            return max(nums[0], nums[1]);
+        }
+
+        vector<int> dp(101);
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+
+        for (int i = 2; i < nums.size(); i++) {
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i]);
+        }
+
+        for (int i = 0; i < dp.size(); i++) {
+            cout << dp[i] << " ";
+        }
+        return dp[nums.size() - 1];
+    }
+};
